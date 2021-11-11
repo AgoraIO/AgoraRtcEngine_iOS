5df055a0d19aa76f7c30ca8f07c6878e4995acff
Tom Brow <tom@tombrow.com>
2021-11-11 09:53:18 -0600

    fix SPM deployment target warning
    Removing the `platforms` parameter avoids this warning in projects that use the Swift package:
    "The iOS Simulator deployment target 'IPHONEOS_DEPLOYMENT_TARGET' is set to 8.0, but the range of supported deployment target versions is 9.0 to 15.0.99."

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "AgoraRtcKit",
     defaultLocalization: "en",
-    platforms: [.iOS(.v8)],
     products: [
         .library(
             name: "AgoraRtcKit",
